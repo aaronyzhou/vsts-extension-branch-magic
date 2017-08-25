63b40e6da0d1a9a0281ee5b10a0a595b9dbd65a7
Aaron Zhou <aazhou@microsoft.com>
2017-08-25 16:46:01 -0700

    Implement merge master

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,168 @@
-define(function(){return function(n){function e(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return n[r].call(o.exports,o,o.exports,e),o.l=!0,o.exports}var t={};return e.m=n,e.c=t,e.i=function(n){return n},e.d=function(n,t,r){e.o(n,t)||Object.defineProperty(n,t,{configurable:!1,enumerable:!0,get:r})},e.n=function(n){var t=n&&n.__esModule?function(){return n.default}:function(){return n};return e.d(t,"a",t),t},e.o=function(n,e){return Object.prototype.hasOwnProperty.call(n,e)},e.p="",e(e.s=0)}([function(n,e){var t={getMenuItems:function(n){return[{icon:"fabric://BranchMerge",title:"Merge Master",action:function(n){console.log("hi")}}]}};VSS.register(VSS.getContribution().id,t)}])});
+define(["TFS/VersionControl/Contracts","TFS/VersionControl/GitRestClient","VSS/Utils/Core"], function(__WEBPACK_EXTERNAL_MODULE_0__, __WEBPACK_EXTERNAL_MODULE_1__, __WEBPACK_EXTERNAL_MODULE_2__) { return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 3);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_0__;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__, exports, __webpack_require__(1), __webpack_require__(0), __webpack_require__(2)], __WEBPACK_AMD_DEFINE_RESULT__ = function (require, exports, GitRestClient_1, Contracts_1, Core_1) {
+    "use strict";
+    var _this = this;
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var actionProvider = {
+        getMenuItems: function (context) {
+            return [{
+                    icon: "fabric://Merge",
+                    title: "Merge Master",
+                    action: function (actionContext) {
+                        console.log(actionContext);
+                        // If we are calling from the files page
+                        if (actionContext.getSourceItemContext) {
+                            actionContext.getSourceItemContext().then(function (a) {
+                                var repoId = a.gitRepository.id;
+                                var branch = "refs/heads/" + a.version;
+                                makePullRequest(repoId, branch);
+                            });
+                        }
+                        else {
+                            var repoId = actionContext.pullRequest.repositoryId;
+                            var branch = actionContext.pullRequest.sourceRefName;
+                            makePullRequest(repoId, branch);
+                        }
+                    }
+                }];
+        }
+    };
+    var makePullRequest = function (repoId, sourceRefName) {
+        var webContext = VSS.getWebContext();
+        var identityRef = {
+            id: webContext.user.id
+        };
+        var completionOptions = {
+            bypassPolicy: true,
+            bypassReason: "merge master",
+            deleteSourceBranch: false,
+            mergeCommitMessage: "merge master",
+            squashMerge: true
+        };
+        var pullReq = {
+            autoCompleteSetBy: identityRef,
+            description: "merge master",
+            sourceRefName: "refs/heads/master",
+            targetRefName: sourceRefName,
+            completionOptions: completionOptions,
+            title: "merge master"
+        };
+        GitRestClient_1.getClient().createPullRequest(pullReq, repoId, null, false, false).then(function (value) {
+            pollMergeStatus(value);
+        });
+    };
+    var pollMergeStatus = function (pullReq) {
+        GitRestClient_1.getClient().getPullRequest(pullReq.repository.id, pullReq.pullRequestId).then(function (pr) {
+            if (pr.mergeStatus === Contracts_1.PullRequestAsyncStatus.Succeeded) {
+                var completeStatus = {
+                    lastMergeSourceCommit: pr.lastMergeSourceCommit,
+                    status: Contracts_1.PullRequestStatus.Completed
+                };
+                GitRestClient_1.getClient().updatePullRequest(completeStatus, pr.repository.id, pr.pullRequestId);
+            }
+            else if (pr.mergeStatus === Contracts_1.PullRequestAsyncStatus.NotSet || pr.mergeStatus === Contracts_1.PullRequestAsyncStatus.Queued) {
+                Core_1.delay(_this, 100, pollMergeStatus, [pr]);
+            }
+            else {
+                console.log("error");
+                return;
+            }
+        });
+    };
+    // Register context menu action provider
+    VSS.register(VSS.getContribution().id, actionProvider);
+}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+
+
+/***/ })
+/******/ ])});;
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,76 @@
- /// <reference types="vss-web-extension-sdk" />
+/// <reference types="vss-web-extension-sdk" />
+import { getClient } from "TFS/VersionControl/GitRestClient";
+import { GitVersionType, GitVersionOptions, GitBaseVersionDescriptor, GitTargetVersionDescriptor, GitCommitDiffs, GitPullRequest, GitPullRequestCompletionOptions, PullRequestStatus, PullRequestAsyncStatus } from "TFS/VersionControl/Contracts";
+import { IdentityRef } from "VSS/WebApi/Contracts";
+import { delay } from "VSS/Utils/Core";
 
 const actionProvider = {
     getMenuItems: (context) => {
         return [<IContributedMenuItem>{
-            icon: "fabric://BranchMerge",
+            icon: "fabric://Merge",
             title: "Merge Master",
             action: (actionContext) => {
-                console.log("hi");
+                console.log(actionContext);
+
+                // If we are calling from the files page
+                if (actionContext.getSourceItemContext) {
+                    actionContext.getSourceItemContext().then((a) => {
+                        const repoId = a.gitRepository.id;
+                        const branch = `refs/heads/${a.version}`;
+                        makePullRequest(repoId, branch);
+                    });
+                } else {
+                    const repoId = actionContext.pullRequest.repositoryId;
+                    const branch = actionContext.pullRequest.sourceRefName;
+                    makePullRequest(repoId, branch);
+                }
             }
         }];
     }
+};
+
+const makePullRequest = (repoId: string, sourceRefName: string) => {
+    const webContext = VSS.getWebContext();
+    const identityRef: IdentityRef = {
+        id: webContext.user.id
+    } as IdentityRef;
+
+    const completionOptions: GitPullRequestCompletionOptions = {
+        bypassPolicy: true,
+        bypassReason: "merge master",
+        deleteSourceBranch: false,
+        mergeCommitMessage: "merge master",
+        squashMerge: true
+    };
+
+    const pullReq: GitPullRequest = {
+        autoCompleteSetBy: identityRef,
+        description: "merge master",
+        sourceRefName: "refs/heads/master",
+        targetRefName: sourceRefName,
+        completionOptions: completionOptions,
+        title: "merge master"
+    } as GitPullRequest;
+    getClient().createPullRequest(pullReq, repoId, null, false, false).then((value: GitPullRequest) => {
+        pollMergeStatus(value);
+    });
+};
+
+const pollMergeStatus = (pullReq: GitPullRequest) => {
+    getClient().getPullRequest(pullReq.repository.id, pullReq.pullRequestId).then((pr: GitPullRequest) => {
+        if (pr.mergeStatus === PullRequestAsyncStatus.Succeeded) {
+            const completeStatus: GitPullRequest = {
+                lastMergeSourceCommit: pr.lastMergeSourceCommit,
+                status: PullRequestStatus.Completed
+            } as GitPullRequest;
+            getClient().updatePullRequest(completeStatus, pr.repository.id, pr.pullRequestId);
+        } else if (pr.mergeStatus === PullRequestAsyncStatus.NotSet || pr.mergeStatus === PullRequestAsyncStatus.Queued) {
+            delay(this, 100, pollMergeStatus, [pr]);
+        } else {
+            console.log("error");
+            return;
+        }
+    });
 };
 
 // Register context menu action provider
